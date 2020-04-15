f7a8e323ad1d5f40563205e569291dde8262b8cd
dwuggh <dwuggh@gmail.com>
2020-04-15 23:21:36 +0800

    configure agenda location

--- init.el
+++ init.el
@@ -46,7 +46,10 @@ This function should only modify configuration layer settings."
      lsp
      markdown
      multiple-cursors
-     org
+     ( org
+       :variables
+       org-agenda-files '("~/org/agenda")
+       )
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
