10c4394615dfb644e1ecc8c7b2a9f5e77f581edc
dwuggh <dwuggh@gmail.com>
2020-06-11 16:54:04 +0800

    commit 5fcd84d of spacemacs latex layer

--- mylatex/config.el
+++ mylatex/config.el
@@ -11,7 +11,30 @@
 
 ;; variables
 
-(spacemacs|define-jump-handlers latex-mode)
+;; commit 5fcd84d
+;; AUCTeX is weird: It reports major-mode as latex-mode (since TeX-latex-mode is
+;; applied as an :override advice on the basic built-in latex-mode), but its mode
+;; hook is LaTeX-mode-hook, not latex-mode-hook (which is only run by the built-in
+;; latex-mode). Since bind-map uses the value of major-mode, we must pass
+;; latex-mode to spacemacs|define-jump-handlers. But then
+;; spacemacs//init-jump-handlers-latex-mode gets added to latex-mode-hook, which
+;; never gets run. So we must manualy add it to LaTeX-mode-hook.
+
+;; [latex] Use dumb-jump as primary jump handler
+
+;; Otherwise, the default is used, which prioritizes evil-goto-definition over
+;; dumb-jump-go. Dumb Jump tends to Just Work, while evil-goto-definition doesn't
+;; handle LaTeX very well, at least not without a TAGS table.
+
+;; (spacemacs|define-jump-handlers latex-mode)
+
+;; Even though AUCTeX uses TeX-latex-mode rather than latex-mode, major-mode
+;; will still be bound to 'latex-mode (since AUCT
+;; latex-mode with TeX-latex-mode), so the keymap's name should use the
+;; lowercase form, since bind-map uses the value of major-mode...
+(spacemacs|define-jump-handlers latex-mode dumb-jump-go)
+;; ...but AUCTeX runs LaTeX-mode-hook rather than latex-mode-hook, so:
+(add-hook 'LaTeX-mode-hook #'spacemacs//init-jump-handlers-latex-mode)
 
 (defvar latex-build-command (if (executable-find "latexmk") "LatexMk" "LaTeX")
   "The default command to use with `SPC m b'")
